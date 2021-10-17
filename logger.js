d1b4a1247d2c821a7320ddc52aa3ee82fbe677d7
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-10-17 19:52:14 -0400

    changing from JSON to normal for easier pulling

--- logger.js
+++ logger.js
@@ -1,30 +1,26 @@
-// function pushHome(data)
-// {
-//     var url = "https://7fd8-2620-101-f000-2c03-00-c5d.ngrok.io/messageReciever";
-//     $.ajax({
-//         type: "POST",
-//         url: url,
-//         data: data,
-//         dataType: 'json'
-//     });
-// }
+$.get("https://taggagii.github.io/home.html", function(home) {
+    function pushHome(data)
+    {
+        $.ajax({
+            type: "POST",
+            url: home,
+            data: data,
+            dataType: 'json'
+        });
+    }
 
+    $.getJSON("https://api.ipify.org?format=json", function(ipv4) {
+        $.getJSON("https://api64.ipify.org?format=json", function(ipv6) {
+            if (ipv4.ip === ipv6.ip)
+                ipv6.ip = null;
 
-// $.getJSON("https://api.ipify.org?format=json", function(ipv4) {
-//     $.getJSON("https://api64.ipify.org?format=json", function(ipv6) {
-//         if (ipv4.ip === ipv6.ip)
-//             ipv6.ip = null;
+            let data = {
+                date: new Date(),
+                ipv4: ipv4.ip,
+                ipv6: ipv6.ip
+            }
 
-//         let data = {
-//             date: new Date(),
-//             ipv4: ipv4.ip,
-//             ipv6: ipv6.ip
-//         }
-//         pushHome();
-//     });
-// });
-
-$.getJSON("https://taggagii.github.io/home.html", function(data) {
-    console.log(data);
-    console.log("what");
-})
+            pushHome(data);
+        });
+    });
+});
