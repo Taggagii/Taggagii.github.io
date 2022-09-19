0589ad17756623dfa043c7a99379c131b51018e6
Joshua Dietrich <Jdietrich0603@gmail.com>
2022-09-19 06:03:45 -0400

    adding some styling to the webpage when the api is active

--- dynamicGithubPages/toPullFrom/index.js
+++ dynamicGithubPages/toPullFrom/index.js
@@ -79,7 +79,7 @@ app.get('/ngrok', async (req, res) => {
 
 app.get('/', (req, res) => {
     res.status(200).send({
-        youDidItRight: 'the message that you\'re reading was served by an express api running through ngrok',
+        youDidItRight: `<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 4vw; font-family: Arial;">This page is cool because everything about it was styled through an API, to prove this, here's a random number: ${Math.random()} </br>Try refreshing the page.</div>`,
     });
 });
 
