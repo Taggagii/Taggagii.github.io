a914c404541fb6f3bd0f66c680bc9f4db02ceec0
Josh Dietrich <jdietrich0603@gmail.com>
2024-04-14 21:14:05 -0400

    fixing background

--- asteroids/main.js
+++ asteroids/main.js
@@ -125,7 +125,8 @@ var keys = {
 
 function clear()
 {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function clearBlack()
